32732045e089b335b0f5f1278c753e3f9098a246
qwerty250 <qiyue2001@gmail.com>
2021-08-16 23:30:52 +0800

    init with Exp01
    Exp01: 用Qt库读取图像，输出长宽和文件大小，输入坐标输出灰度值。

---
+++ Exp01/main.cpp
@@ -0,0 +1,33 @@
+﻿#include "mainwindow.h"
+
+#include <QApplication>
+
+#include <QTextCodec>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    //设置中文字体
+    a.setFont(QFont("Microsoft Yahei", 9));
+
+    //设置中文编码
+    #if (QT_VERSION <= QT_VERSION_CHECK(5,0,0))
+    #if _MSC_VER
+    QTextCodec *codec = QTextCodec::codecForName("gbk");
+    #else
+    QTextCodec *codec = QTextCodec::codecForName("utf-8");
+    #endif
+    QTextCodec::setCodecForLocale(codec);
+    QTextCodec::setCodecForCStrings(codec);
+    QTextCodec::setCodecForTr(codec);
+    #else
+    QTextCodec *codec = QTextCodec::codecForName("utf-8");
+    QTextCodec::setCodecForLocale(codec);
+    #endif
+
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
---
+++ Exp01/mainwindow.cpp
@@ -0,0 +1,103 @@
+﻿/* FILE ENCODING is UTF8, no need QString::fromLocal8Bit() */
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QMainWindow>
+#include <QFile>
+#include <QMessageBox>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QStandardItemModel>
+#include <QImageReader>
+#include <QDebug>
+
+QImage image;
+
+void MainWindow::ClickButton(){
+    auto row = ui->plainTextEdit_row->toPlainText().toUInt();
+    auto column = ui->plainTextEdit_column->toPlainText().toUInt();
+    auto width = image.width();
+    auto height = image.height();
+    // row, column starting from 0
+    if(row >= width || column >= height || row < 0 || column < 0){
+        QMessageBox mesg;
+        mesg.warning(this, QStringLiteral("Warning"), QStringLiteral("Width or height exceeds the bound."));
+    }
+    else {
+        //This is WRONG: uchar g = image.bits()[(row)*width+(column)];
+        uchar g = image.pixel(column, row);
+        ui->label_grey_val->setText(QString::number(g));
+    }
+}
+
+
+void MainWindow::loadFile(const QString filename) {
+    // 读取图像
+    QImageReader reader;
+    reader.setFileName(filename);
+
+    if (reader.canRead())
+    {
+        // 读取图片
+        // 以单通道灰度格式读取
+        image = reader.read().convertToFormat(QImage::Format_Grayscale8);
+
+        // 获取描述信息
+        QStringList keys = reader.textKeys();
+        QString strValue("");
+        foreach (QString strKey, keys)
+        {
+            strValue = reader.text(strKey).toLocal8Bit();
+            qDebug() << QString("key : %1  value : %2").arg(strKey).arg(strValue);
+        }
+
+        QString info = QString("image size: %1 \n width: %2 \n height: %3")
+                .arg(image.sizeInBytes())
+                .arg(image.width())
+                .arg(image.height());
+
+        ui->label_info->setText(info);
+        //QMessageBox mesg;
+        //mesg.information(this, "Open image", info);
+
+    }
+    else
+    {
+        // 获取错误信息
+        QImageReader::ImageReaderError error = reader.error();
+        QString strError = reader.errorString();
+        qDebug() << "Last Error : " << strError;
+    }
+
+}
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    QString fileName = QFileDialog::getOpenFileName(
+        this, tr("open image file"),
+        "./", tr("Image files(*.bmp *.jpg *.pbm *.pgm *.png *.ppm *.xbm *.xpm);;All files (*.*)"));
+
+    if (fileName.isEmpty())
+    {
+        QMessageBox mesg;
+        mesg.warning(this, QStringLiteral("Warning"), QStringLiteral("Unable to open the image!"));
+        return;
+    }
+    else{
+        loadFile(fileName);
+    }
+
+    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(ClickButton()));
+
+    ui->label->resize(image.width(),image.height());
+    ui->label->setPixmap(QPixmap::fromImage(image));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
